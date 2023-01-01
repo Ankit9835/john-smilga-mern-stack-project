6f190bfa0940d049d17608a50d4c544ec3dc1176
Ankit9835 <ankydchrismatic@gmail.com>
2023-01-01 22:03:23 +0530

    get jobs module working on

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -1,9 +1,20 @@
-const createJob = (req,res) => {
-    res.send('create job')
+import Job from '../models/Job.js'
+import {StatusCodes} from 'http-status-codes'
+import {BadRequest, UnAuthenticated} from '../errors/index.js'
+
+const createJob = async (req,res) => {
+   const {position, company} = req.body
+   if(!position || !company){
+      throw new BadRequest('please provide all values')
+   }
+   req.body.createdBy = req.user.userId
+   const job = await Job.create(req.body)
+    res.status(StatusCodes.CREATED).json({job})
 }
 
-const getAllJob = (req,res) => {
-    res.send('get all jobs')
+const getAllJob = async (req,res) => {
+   const jobs= await Job.find({createdBy:req.user.userId})
+   res.status(StatusCodes.OK).json({jobs,totalJobs:jobs.length,numOfPages:1})
 }
 
  const updateJob = (req,res) => {
