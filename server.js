0a41c17613a3f87797a314a0473ebef5716d7e83
Ankit9835 <ankydchrismatic@gmail.com>
2023-01-04 12:05:11 +0530

    project done

--- server.js
+++ server.js
@@ -10,14 +10,26 @@ import authRouter from './routes/authRoutes.js'
 import jobRouter from './routes/jobRoutes.js'
 import morgan from 'morgan';
 import authenticateUser from './middleware/auth.js'
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import path from 'path';
+import helmet from 'helmet';
+import xss from 'xss-clean';
+import mongoSanitize from 'express-mongo-sanitize';
+
 
 if (process.env.NODE_ENV !== 'production') {
     app.use(morgan('dev'));
   }
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
+app.use(express.static(path.resolve(__dirname, './client/build')));
+
 app.use(express.json())
 
-
+app.use(helmet());
+app.use(xss());
+app.use(mongoSanitize());
 
 app.get('/', (req,res) => {
     //throw new Error('new error')
@@ -31,6 +43,10 @@ app.get('/api/v1', (req,res) => {
 
 app.use('/api/v1/auth',authRouter)
 app.use('/api/v1/jobs',authenticateUser,jobRouter)
+
+app.get('*', function (request, response) {
+    response.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
+  });
 
 app.use(notFoundMiddleware)
 app.use(errorHandler)
